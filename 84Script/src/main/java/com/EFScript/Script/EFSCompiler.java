78642b20e39bcb04506a92f5f9055bd99df3bf50
DisabledMallis <mallisiscool@gmail.com>
2021-03-17 20:41:01 -0400

    Update EFSCompiler.java

--- 84Script/src/main/java/com/EFScript/Script/EFSCompiler.java
+++ 84Script/src/main/java/com/EFScript/Script/EFSCompiler.java
@@ -1,20 +1,26 @@
 package com.EFScript.Script;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.EFScript.Logger;
 
-import java.nio.file.Paths;
-import org.antlr.v4.runtime.tree.ParseTree;
-import org.antlr.v4.runtime.ParserInterpreter;
-import org.antlr.v4.runtime.CommonTokenStream;
+import com.EFScript.Antlr.EFScriptLexer;
+import com.EFScript.Antlr.EFScriptParser;
+
+import org.antlr.runtime.tree.ParseTree;
+import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.CharStreams;
-import org.antlr.v4.runtime.LexerInterpreter;
-import org.antlr.v4.tool.Grammar;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.Token;
 
 public class EFSCompiler
 {
 	public EFSCompiler(String code)
 	{
-		
+		EFScriptLexer lexer = new EFScriptLexer(CharStreams.fromString(code));
+		EFScriptParser parser = new EFScriptParser(new CommonTokenStream(lexer));
+		ParseTree tree = parser.parse();
 	}
 
 	public byte[] compile()
