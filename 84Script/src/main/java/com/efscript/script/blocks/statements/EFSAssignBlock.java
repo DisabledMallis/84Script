944bb151165fedca8e20390339f895e8e8ea7945
DisabledMallis <mallisiscool@gmail.com>
2021-03-28 16:07:13 -0400

    Update EFSAssignBlock.java

--- 84Script/src/main/java/com/efscript/script/blocks/statements/EFSAssignBlock.java
+++ 84Script/src/main/java/com/efscript/script/blocks/statements/EFSAssignBlock.java
@@ -1,7 +1,12 @@
 package com.efscript.script.blocks.statements;
 
 import com.efscript.antlr.EFScriptParser.Assign_stmtContext;
+import com.efscript.antlr.EFScriptParser.ExpressionContext;
+import com.efscript.antlr.EFScriptParser.IdentifierContext;
+import com.efscript.script.blocks.EFSExpressionBlock;
 import com.efscript.script.blocks.EFSStatementBlock;
+import com.efscript.script.blocks.EFSVarToken;
+import com.efscript.ti.TiCompiler;
 import com.efscript.ti.TiToken;
 
 public class EFSAssignBlock extends EFSStatementBlock<Assign_stmtContext> {
@@ -12,7 +17,31 @@ public class EFSAssignBlock extends EFSStatementBlock<Assign_stmtContext> {
 
 	@Override
 	public TiToken[] compile() {
-		return null;
+		// Initialize TiCompiler
+		TiCompiler minComp = new TiCompiler();
+
+		/*
+		 * Generated output should be: <expression>->L1(X)
+		 */
+
+		// Get context
+		Assign_stmtContext aCtx = this.getCtx();
+		// Expression to evaluate & store
+		ExpressionContext eCtx = aCtx.expression();
+		// Create an expression block instance
+		EFSExpressionBlock<ExpressionContext> exprBlock = new EFSExpressionBlock<ExpressionContext>(eCtx);
+		// Compile it
+		minComp.appendInstruction(exprBlock.compile());
+		// Store token
+		minComp.appendInstruction(TiToken.STORE);
+		// Get the identifier
+		IdentifierContext iCtx = aCtx.identifier();
+		// Get the var accessor to store the expression into
+		EFSVarToken vTok = new EFSVarToken(iCtx.getText());
+		// Compile the accessor
+		minComp.appendInstruction(vTok.compile());
+		// Return the result
+		return minComp.getTokens();
 	}
-	
+
 }
