e5437ec2c58b74a4bf17a69e1251cd806f088f4e
DisabledMallis <mallisiscool@gmail.com>
2021-03-28 15:07:35 -0400

    Update EFSDecBlock.java

--- 84Script/src/main/java/com/efscript/script/blocks/statements/EFSDecBlock.java
+++ 84Script/src/main/java/com/efscript/script/blocks/statements/EFSDecBlock.java
@@ -1,18 +1,41 @@
 package com.efscript.script.blocks.statements;
 
 import com.efscript.antlr.EFScriptParser.Dec_stmtContext;
+import com.efscript.antlr.EFScriptParser.IdentifierContext;
 import com.efscript.script.blocks.EFSStatementBlock;
+import com.efscript.script.blocks.EFSVarToken;
+import com.efscript.ti.TiCompiler;
 import com.efscript.ti.TiToken;
 
 public class EFSDecBlock extends EFSStatementBlock<Dec_stmtContext> {
 
-	public EFSDecBlock(Dec_stmtContext ctx)
-	{
+	public EFSDecBlock(Dec_stmtContext ctx) {
 		super(ctx);
 	}
 
 	@Override
 	public TiToken[] compile() {
-		return null;
+		TiCompiler miniTiComp = new TiCompiler();
+		// Get the context
+		Dec_stmtContext decCtx = this.getCtx();
+		// Get the identifier
+		IdentifierContext iCtx = decCtx.identifier();
+		// The identifier must be a var, and we can assume this.
+		EFSVarToken vToken = new EFSVarToken(iCtx.getText());
+
+		/*
+		 * Should compile to: L1(X)-1->L1(X)
+		 */
+		// Append the accessor
+		miniTiComp.appendInstruction(vToken.compile());
+		// Subtract token
+		miniTiComp.appendInstruction(TiToken.SUBTRACT);
+		// Num 1
+		miniTiComp.appendInstruction(TiToken.NUM_1);
+		// Store token (->)
+		miniTiComp.appendInstruction(TiToken.STORE);
+		// The accessor
+		miniTiComp.appendInstruction(vToken.compile());
+		return miniTiComp.getTokens();
 	}
 }
