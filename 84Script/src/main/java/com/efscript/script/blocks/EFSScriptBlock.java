39aa95e854708d65432808e9467da9ec15c19bb5
DisabledMallis <mallisiscool@gmail.com>
2021-10-09 17:39:22 -0400

    Update EFSScriptBlock.java

--- 84Script/src/main/java/com/efscript/script/blocks/EFSScriptBlock.java
+++ 84Script/src/main/java/com/efscript/script/blocks/EFSScriptBlock.java
@@ -22,56 +22,102 @@ public class EFSScriptBlock extends ABlock<ScriptContext> {
 		String scriptMetaName = ctx.header().meta().scriptName().identifier().getText();
 		Context.pushContext(new Context(scriptMetaName));
 
+		// Pre initialization
+		// Due to the fact we use matrices now, we need to check the
+		// dimensions of the matrix to see if its even defined. If its undefined, we should
+		// define it with 1x1 matrix filled with 0. From then on, the rest of the program
+		// should safely size the table.
+		/*
+		 * dim([J])->L1
+		 * If L1(1)=0 or L1(2)=0
+		 * Then
+		 * [[0]]->[J]
+		 * End
+		 */
+
 		// Initialization section
 		// Basically just reset the registers
 		// (And hope I=0, otherwise the program will be die)
 		/*
-		 * If I=0 Then 0→G 0→F 0→C 1→I End
+		 * If [J](1,1)=0
+		 * Then
+		 * 1->[J](1,1)
+		 * 0->[J](1,2)
+		 * 0->[J](1,3)
+		 * 0->[J](1,4)
+		 * 1->[J](1,5)
+		 * End
 		 */
-		// IF I=0
+		// IF [J](1,1)=0
 		// Then
 		comp.appendInstruction(TiToken.IF);
-		comp.appendInstruction(TiToken.LETTER_I);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
 		comp.appendInstruction(TiToken.EQUALS);
 		comp.appendInstruction(TiToken.NUM_0);
 		comp.appendInstruction(TiToken.NEWLINE);
 		comp.appendInstruction(TiToken.THEN);
 		comp.appendInstruction(TiToken.NEWLINE);
 		/*
-		 * 0→G
-		 * 0→F
-		 * 0→C
-		 * ClrList L1
-		 * ClrList L2
-		 * 1→I 
+		 * 1->[J](1,1)
+		 * 0->[J](1,2)
+		 * 0->[J](1,3)
+		 * 0->[J](1,4)
+		 * 1->[J](1,5)
 		 */
-		// 0→G
-		comp.appendInstruction(TiToken.NUM_0);
-		comp.appendInstruction(TiToken.STORE);
-		comp.appendInstruction(TiToken.LETTER_G);
-		comp.appendInstruction(TiToken.NEWLINE);
-		// 0→F
-		comp.appendInstruction(TiToken.NUM_0);
-		comp.appendInstruction(TiToken.STORE);
-		comp.appendInstruction(TiToken.LETTER_F);
-		comp.appendInstruction(TiToken.NEWLINE);
-		// 0→C
-		comp.appendInstruction(TiToken.NUM_0);
-		comp.appendInstruction(TiToken.STORE);
-		comp.appendInstruction(TiToken.LETTER_C);
-		comp.appendInstruction(TiToken.NEWLINE);
-		//ClrList L1
-		comp.appendInstruction(TiToken.CLR_LIST);
-		comp.appendInstruction(TiToken.LIST1);
-		comp.appendInstruction(TiToken.NEWLINE);
-		//ClrList L2
-		comp.appendInstruction(TiToken.CLR_LIST);
-		comp.appendInstruction(TiToken.LIST2);
-		comp.appendInstruction(TiToken.NEWLINE);
-		// 1→I
+		// 1->[J](1,1)
 		comp.appendInstruction(TiToken.NUM_1);
 		comp.appendInstruction(TiToken.STORE);
-		comp.appendInstruction(TiToken.LETTER_I);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
+		comp.appendInstruction(TiToken.NEWLINE);
+		// 0->[J](1,2)
+		comp.appendInstruction(TiToken.NUM_0);
+		comp.appendInstruction(TiToken.STORE);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_2);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
+		comp.appendInstruction(TiToken.NEWLINE);
+		// 0->[J](1,3)
+		comp.appendInstruction(TiToken.NUM_0);
+		comp.appendInstruction(TiToken.STORE);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_3);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
+		comp.appendInstruction(TiToken.NEWLINE);
+		// 0->[J](1,4)
+		comp.appendInstruction(TiToken.NUM_0);
+		comp.appendInstruction(TiToken.STORE);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_4);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
+		comp.appendInstruction(TiToken.NEWLINE);
+		// 1->[J](1,5)
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.STORE);
+		comp.appendInstruction(TiToken.MATRIX_J);
+		comp.appendInstruction(TiToken.OPEN_BRACKET);
+		comp.appendInstruction(TiToken.NUM_1);
+		comp.appendInstruction(TiToken.COMMA);
+		comp.appendInstruction(TiToken.NUM_5);
+		comp.appendInstruction(TiToken.CLOSE_BRACKET);
 		comp.appendInstruction(TiToken.NEWLINE);
 		// END
 		comp.appendInstruction(TiToken.END);
@@ -79,14 +125,16 @@ public class EFSScriptBlock extends ABlock<ScriptContext> {
 
 		// Function table
 		/*
-		 * //G determines if a func is called If G>0 -|---------------Funcs must be
-		 * defined at thr top of the script Then -| //F is which function If F=0 Then
+		 * //[J](1,3) determines if a func is called
+		 * If [J](1,3)>0
+		 * //Funcs must be defined at the top of the script
+		 * Then
+		 * //F is which function If F=0 Then
 		 * //L₂ is the parameter stack L₂(1)+L₂(2)→C
-		 * 
 		 * End
-		 * 
 		 * End
 		 */
+		//TODO: Finish updating this file
 		comp.appendInstruction(TiToken.IF);
 		comp.appendInstruction(TiToken.LETTER_G);
 		comp.appendInstruction(TiToken.GREATER_THAN);
